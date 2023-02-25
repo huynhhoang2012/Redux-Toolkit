1ab8cbb403a085db8fbdabf1e48494f803dbcc4e
Hoang Huynh <hoanghuynh@Hoang-Huynh.local>
2023-02-26 02:01:19 +0700

    update code

--- scripts/pre-install.js
+++ scripts/pre-install.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
-const childProcess = require('child_process');
-const os = require('os');
+const childProcess = require("child_process");
+const os = require("os");
 
 [
   // Kill the metro bundler if it's running.
   {
     command: 'pkill -f "cli.js start" || set exit 0',
-    onlyPlatforms: ['darwin', 'linux'],
+    onlyPlatforms: ["darwin", "linux"],
   },
 ]
   .filter(
-    ({onlyPlatforms}) =>
-      !onlyPlatforms || onlyPlatforms.includes(os.platform()),
+    ({ onlyPlatforms }) =>
+      !onlyPlatforms || onlyPlatforms.includes(os.platform())
   )
-  .forEach(commandAndOptions => {
-    const {command, onlyPlatform: _, ...options} = commandAndOptions;
+  .forEach((commandAndOptions) => {
+    const { command, onlyPlatform: _, ...options } = commandAndOptions;
     try {
       childProcess.execSync(command, {
-        stdio: 'inherit',
+        stdio: "inherit",
         ...options,
       });
     } catch (error) {
---
+++ src/screen/Demo/index.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import style from "./style";
+
+const DemoScreen = () => {
+  return (
+    <View styles={style.container}>
+      <Text>DemoScreen</Text>
+    </View>
+  );
+};
+
+export default DemoScreen;
